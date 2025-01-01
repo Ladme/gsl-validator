8324a4186fbb2f9987a71c3d86dbcb58ebff9456
Ladme <Ladme@users.noreply.github.com>
2025-01-01 10:10:22 +0000

    deploy: a8800322f7c91222ed6a79b842e5aeb4311fac2d

---
+++ gsl_validator.js
@@ -0,0 +1,4 @@
+import * as wasm from "./gsl_validator_bg.wasm";
+export * from "./gsl_validator_bg.js";
+import { __wbg_set_wasm } from "./gsl_validator_bg.js";
+__wbg_set_wasm(wasm);
---
+++ gsl_validator_bg.wasm.d.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __errno_location: () => number;
+export const malloc: (a: number) => number;
+export const free: (a: number) => void;
+export const htonl: (a: number) => number;
+export const htons: (a: number) => number;
+export const emscripten_main_thread_process_queued_calls: () => void;
+export const ntohs: (a: number) => number;
+export const _start: () => void;
+export const setThrew: (a: number, b: number) => void;
+export const stackSave: () => number;
+export const stackRestore: (a: number) => void;
+export const stackAlloc: (a: number) => number;
+export const __cxa_can_catch: (a: number, b: number, c: number) => number;
+export const __cxa_is_pointer_type: (a: number) => number;
